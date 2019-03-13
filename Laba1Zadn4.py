3e97599774733ca3b6631b886f8dadd8643a692d
Alexandrmrn <48490798+Alexandrmrn@users.noreply.github.com>
2019-03-13 10:20:39 +0300

    Update Laba1Zadn4.py

--- Laba1Zadn4.py
+++ Laba1Zadn4.py
@@ -1,6 +1,6 @@
 
 a = [int(input('Input number ' + str(i) + ': ')) for i in range(1, 11)]
 print(a)
-
-b = [x for x in a if x % 2 != 0] + [x for x in a if x % 2 == 0]
+c = [a[1] for i in range(1, 6)]
+b = [x for x in a if x % 2 != 0] + с
 print(b)
